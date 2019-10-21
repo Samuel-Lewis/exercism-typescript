df5630d6d668d67f0f6090dafc12d8bcbeba1da0
Samuel Lewis <slewis@atlassian.com>
2019-10-21 14:34:35 +1100

    Exercise 3 - wip variation

--- exercises/leap/leap.ts
+++ exercises/leap/leap.ts
@@ -1,5 +1,18 @@
-function isLeapYear( /* Parameters go here */ ) {
-    // Your code here
+function isLeapYear(year: number): boolean {
+  //   return !(year % 100 === 0 && year % 400 !== 0) || year % 4 === 0;
+  if (year % 400 === 0) {
+    return true;
+  }
+
+  if (year % 100 === 0) {
+    return false;
+  }
+
+  if (year % 4 === 0) {
+    return true;
+  }
+
+  return false;
 }
 
-export default isLeapYear
+export default isLeapYear;
