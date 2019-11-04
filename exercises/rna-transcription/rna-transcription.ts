144dae69b4db6dcaffbefe2f1952486e1c39c514
Samuel Lewis <slewis@atlassian.com>
2019-11-04 15:10:17 +1100

    Ex 4 - wip

--- exercises/rna-transcription/rna-transcription.ts
+++ exercises/rna-transcription/rna-transcription.ts
@@ -1,7 +1,42 @@
 class Transcriptor {
-    toRna( /* Parameters go here */ ) {
-        // Your code here
-    }
+  toRna(dna: string): string {
+    // if (dna === '') {
+    //   return dna;
+    // }
+    // const firstChar = dna[0];
+
+    // if (firstChar === 'C') {
+    //   return 'G' + this.toRna(dna.slice(1));
+    // }
+    // if (firstChar === 'G') {
+    //   return 'C' + this.toRna(dna.slice(1));
+    // }
+    // if (firstChar === 'A') {
+    //   return 'U' + this.toRna(dna.slice(1));
+    // }
+    // if (firstChar === 'T') {
+    //   return 'A' + this.toRna(dna.slice(1));
+    // }
+
+    return dna
+      .split('')
+      .map(d => {
+        if (d === 'C') {
+          return 'G';
+        }
+        if (d === 'G') {
+          return 'C';
+        }
+        if (d === 'A') {
+          return 'U';
+        }
+        if (d === 'T') {
+          return 'A';
+        }
+        throw new TypeError('not DNA');
+      })
+      .join('');
+  }
 }
 
-export default Transcriptor
+export default Transcriptor;
