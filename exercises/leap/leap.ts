1a5c06e74bea10b6cba66b6ada70ccf2fc28d0b1
Samuel Lewis <slewis@atlassian.com>
2019-11-04 14:25:52 +1100

    Ex 3 - one liner

--- exercises/leap/leap.ts
+++ exercises/leap/leap.ts
@@ -1,18 +1,5 @@
 function isLeapYear(year: number): boolean {
-  //   return !(year % 100 === 0 && year % 400 !== 0) || year % 4 === 0;
-  if (year % 400 === 0) {
-    return true;
-  }
-
-  if (year % 100 === 0) {
-    return false;
-  }
-
-  if (year % 4 === 0) {
-    return true;
-  }
-
-  return false;
+  return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
 }
 
 export default isLeapYear;
