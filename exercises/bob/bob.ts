fd016017b5f52dbfd3cbb3c1bf651dbc946095c1
Samuel Lewis <slewis@atlassian.com>
2019-11-26 15:03:49 +1100

    Ex 7 - bob

--- exercises/bob/bob.ts
+++ exercises/bob/bob.ts
@@ -1,7 +1,24 @@
 class Bob {
-    hey( /* Parameters go here */ ) {
-        // Your code here
+  hey(phrase: string) {
+    phrase = phrase.trim();
+
+    const upper = phrase.toUpperCase();
+    const lower = phrase.toLowerCase();
+    const shouting = upper === phrase && upper !== lower;
+    const question = phrase.endsWith('?');
+
+    if (phrase === '') {
+      return 'Fine. Be that way!';
+    } else if (shouting && question) {
+      return "Calm down, I know what I'm doing!";
+    } else if (shouting) {
+      return 'Whoa, chill out!';
+    } else if (question) {
+      return 'Sure.';
     }
+
+    return 'Whatever.';
+  }
 }
 
-export default Bob
+export default Bob;
