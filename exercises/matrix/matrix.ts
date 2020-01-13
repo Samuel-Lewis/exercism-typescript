3b2ad429a15d3dcce3bd01f1747142cb322e6669
Samuel Lewis <slewis@atlassian.com>
2020-01-13 15:05:44 +1100

    Ex 8 WIP

--- exercises/matrix/matrix.ts
+++ exercises/matrix/matrix.ts
@@ -1,7 +1,33 @@
 class Matrix {
-    constructor( /* Parameters go here */ ) {
-        // Your code here
-    }
+  // public rows: number[][] = [];
+  // public columns: number[][] = [];
+
+  private rowLength: number;
+  private data: number[];
+
+  constructor(private matrixString: string) {
+    this.rowLength = matrixString
+      .substring(0, matrixString.indexOf('\n'))
+      .split(' ').length;
+
+    this.data = matrixString
+      .split('\n')
+      .join(' ')
+      .split(' ')
+      .map(v => parseInt(v, 10));
+  }
+
+  public get rows(): number[][] {
+    return new Array(this.data.length / this.rowLength)
+      .fill(true)
+      .map((_, idx) =>
+        this.data.slice(idx * this.rowLength, (idx + 1) * this.rowLength)
+      );
+  }
+
+  public get columns(): number[][] {
+    return [];
+  }
 }
 
-export default Matrix
+export default Matrix;
